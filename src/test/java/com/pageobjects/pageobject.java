007c05b648eacb44f8bde067460fd4e4cba56298
Sireesha Baduru <sireesha.baduru@rabbithyd.com>
2019-11-08 12:28:42 +0530

    added page object

---
+++ src/test/java/com/pageobjects/pageobject.java
@@ -0,0 +1,11 @@
+package com.pageobjects;
+
+public class pageobject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("jghkljbhgfhklgj");
+
+	}
+
+}
